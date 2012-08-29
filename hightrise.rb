813a5318846172bc19ff2b5be4b0a012e62c5242
Ildar Kayumov <darik4308@gmail.com>
2012-08-29 22:37:51 +0400

    Add_note and add_contact work

--- hightrise.rb
+++ hightrise.rb
@@ -5,12 +5,68 @@ require 'bundler/setup'
 
 require 'highrise'
 
-Highrise::Base.site = 'https://fs13.highrisehq.com'
-Highrise::Base.user = '5c4a60575226ba5434ce1573adc55c3a'
+Highrise::Base.site = 'https://rf1.highrisehq.com'
+Highrise::Base.user = '118afb46d954bf7b1e14f36d8df5a7c1'
 Highrise::Base.format = :xml
 
-# puts Highrise::Person.find(:all).first.contact_data.email_addresses.first.address
+class People
+
+
+  def add_new(email)
+    @contact = Highrise::Person.new(:first_name => email, :contact_data => {})
+    @contact.contact_data = {"email-addresses" => {"email-address" => {"address" => email, "location" => "work"}}}
+    @contact.save
+    return self
+  end
+  
+  def initialize()
+    @contact =""
+  end
+
+  def link(email)
+    @contact = Highrise::Person.search(:email => email)[0]
+    return self
+  end
+
+  def notes()
+    @contact.notes
+  end
+
+  def notes?()
+    !@contact.notes.nil?
+  end
+
+  def note_add(note)
+    new_note = @contact.add_note
+    new_note.body = note
+    new_note.save
+  end
+
+  def note_exist?(note)
+    @contact.notes.each do |n|
+      return true if n.body == note
+    end
+    return false
+  end
+end
+
+def contact_exist(email)
+    Highrise::Person.search(:email => email)==[] ? false : true
+end
 
 NOTES = ['note1', 'note2']
-# find or create by email
-# find or create notes
+email="robert@singer.com"
+new_contact = People.new()
+if contact_exist(email)
+  new_contact.link(email)
+  puts "Find contact!"
+else
+  new_contact.add_new(email)
+  puts "Contact is new"
+end
+NOTES.each do |note|
+unless new_contact.note_exist?(note)
+  new_contact.note_add(note)
+  puts "#{note} -- Note added"
+end
+end
