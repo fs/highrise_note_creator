befa4e33a1421917e0085269ceb09d0bc489b17f
b4a4e3 <jonharbakle@gmail.com>
2012-08-30 11:14:27 +0400

    refactoring

--- highrise.rb
+++ highrise.rb
@@ -9,6 +9,7 @@ Highrise::Base.format = :xml
 
 def main(email,*notes)
   person = Highrise::Person.search(email: email)
+  notes.flatten!
   if person.size == 0
     create_person(email,notes)
   else
@@ -16,11 +17,10 @@ def main(email,*notes)
   end
 end
 
-def create_person(email,*notes)
+def create_person(email,notes)
   person = Highrise::Person.new(:first_name => email, :contact_data => {}) 
   person.contact_data = {"email-addresses" => {"email-address" => {"address" => email, "location" => "work"}}}
   person.save
-  notes.flatten!(1)
   if notes.size != 0
     notes.each do |note| 
       Highrise::Note.new("body"=>note, "subject_id"=>person.id, "subject_type"=>"Party", "visible_to"=>"Everyone").save
@@ -30,7 +30,7 @@ end
 
 def create_notes(person,notes)
   notes.each do |note|  
-    if !person.first.notes.any? { |pnote| pnote.body.downcase == note.downcase}
+    if person.first.notes.all? { |pnote| pnote.body.downcase != note.downcase}
       Highrise::Note.new("body"=>note, "subject_id"=>person.first.id, "subject_type"=>"Party", "visible_to"=>"Everyone").save
     end
   end
