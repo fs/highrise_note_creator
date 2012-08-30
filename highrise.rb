797c10ca9d753655be35186cf0ba57f5c2fa79f7
b4a4e3 <jonharbakle@gmail.com>
2012-08-30 10:30:33 +0400

    var fix

--- highrise.rb
+++ highrise.rb
@@ -8,11 +8,11 @@ Highrise::Base.user = '2aa2648366d4aede1bdbce5531149012'
 Highrise::Base.format = :xml
 
 def main(email,*notes)
-  @person = Highrise::Person.search(email: email)
-  if @person.size == 0
+  person = Highrise::Person.search(email: email)
+  if person.size == 0
     create_person(email,notes)
   else
-    create_notes(@person,notes)  
+    create_notes(person,notes)  
   end
 end
 
@@ -30,14 +30,14 @@ end
 
 def create_notes(person,notes)
   notes.each do |note|  
-    if !@person.first.notes.any? { |pnote| pnote.body.downcase == note.downcase}
-      Highrise::Note.new("body"=>note, "subject_id"=>@person.first.id, "subject_type"=>"Party", "visible_to"=>"Everyone").save
+    if !person.first.notes.any? { |pnote| pnote.body.downcase == note.downcase}
+      Highrise::Note.new("body"=>note, "subject_id"=>person.first.id, "subject_type"=>"Party", "visible_to"=>"Everyone").save
     end
   end
 end
 
-main '339@333.com'
-main '339@333.com','note3','note2'
-
+# Usage examples
+#main '339@333.com'
+#main '339@333.com','note3','note2'
 
 
