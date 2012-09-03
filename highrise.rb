69c974c1eb90408b06267fef5186aa4770d20a88
b4a4e3 <jonharbakle@gmail.com>
2012-09-03 20:02:52 +0400

    base script in /lib now

--- highrise.rb
+++ highrise.rb
@@ -2,42 +2,13 @@
 #Username:asdasdaf password:123456
 
 require 'highrise'
+require File.dirname(__FILE__)+'/lib/highrisecreator'
 
-Highrise::Base.site = 'https://namelast.highrisehq.com'
-Highrise::Base.user = '2aa2648366d4aede1bdbce5531149012'
-Highrise::Base.format = :xml
+site = 'https://namelast.highrisehq.com'
+user = '2aa2648366d4aede1bdbce5531149012'
+params = ARGV 
 
-def main(email,*notes)
-  person = Highrise::Person.search(email: email)
-  notes.flatten!
-  if person.size == 0
-    create_person(email,notes)
-  else
-    create_notes(person,notes)  
-  end
-end
-
-def create_person(email,notes)
-  person = Highrise::Person.new(:first_name => email, :contact_data => {}) 
-  person.contact_data = {"email-addresses" => {"email-address" => {"address" => email, "location" => "work"}}}
-  person.save
-  if notes.size != 0
-    notes.each do |note| 
-      Highrise::Note.new("body"=>note, "subject_id"=>person.id, "subject_type"=>"Party", "visible_to"=>"Everyone").save
-    end
-  end
-end
-
-def create_notes(person,notes)
-  notes.each do |note|  
-    if person.first.notes.all? { |pnote| pnote.body.downcase != note.downcase}
-      Highrise::Note.new("body"=>note, "subject_id"=>person.first.id, "subject_type"=>"Party", "visible_to"=>"Everyone").save
-    end
-  end
-end
-
-# Usage examples
-#main '339@333.com'
-#main '339@333.com','note3','note2'
+pers = HighriseCreator.new(site, user, params)
+pers.create
 
 
